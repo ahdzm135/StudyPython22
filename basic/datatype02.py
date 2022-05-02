187021e7e2d19b3f565beb61bce219bb9876cff2
ahdzm135 <ahdzm135@naver.com>
2022-05-02 12:20:17 +0900

    220502 파이썬 기본 문법학습

---
+++ basic/datatype02.py
@@ -0,0 +1,11 @@
+# 문자열
+bruce_eckel = 'Life is short,\n\tYou need Python.' # 탈출문자 \n \t
+print(bruce_eckel)
+
+# 여러줄 문자열
+multi_line = '''Life is short.
+You need python
+And I need C#, too.
+'''
+
+print(multi_line)
---
+++ basic/variable02.py
@@ -0,0 +1,15 @@
+변수값 = 10
+
+print(변수값)
+
+
+a = 10
+b = 20
+c = 30
+
+print(a == b)
+print(a is b)
+
+a = b
+print(a == b) # 값이 같은지
+print(a is b) # 같은 객체인지
