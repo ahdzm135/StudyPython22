5493930e59bee286f53c789c127b25fb543ed53b
ahdzm135 <ahdzm135@naver.com>
2022-05-03 11:47:57 +0900

    220503 점심전 커밋

--- basic/star_print.py
+++ basic/star_print.py
@@ -1,9 +1,12 @@
 # * 찍기
 for x in range(1, 6):
-    for y in range(x, 6):
+    for y in range(x, 5):
         print('*', end='')
     print('')
 
+    for y in range(1, x):
+        print('x', end='')
+    print('')
 
 print('Hello', end= ' ')
 print('World')
