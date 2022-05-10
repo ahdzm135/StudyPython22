393db129c4ee1118d521a5d771a233e9658a749e
ahdzm135 <ahdzm135@naver.com>
2022-05-10 09:04:49 +0900

    220509 집으로

--- middle/csv_read.py
+++ middle/csv_read.py
@@ -3,7 +3,7 @@ import csv
 
 file_name = './busanbus_211231.csv'
 
-f = open(file_name, mode='r', encoding='cp949')
+f = open(file_name, mode='r', encoding='utf-8')
 reader = csv.reader(f, delimiter=',') # 구분자가 ,일경우
 
 next(reader) # 제목줄 있을 때 필요없을경우
